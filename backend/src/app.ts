d7e060c09ff455617b3cfd58068fa7c351892f7b
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-11-27 08:14:45 -0300

    configured biome

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,11 @@
 import Fastify from 'fastify';
-import type {FastifyInstance} from 'fastify'
-import routes from "./routes";
+import type { FastifyInstance } from 'fastify';
+import routes from './routes';
 
-const app:FastifyInstance = Fastify({
-    logger: true
-})
+const app: FastifyInstance = Fastify({
+  logger: true,
+});
 
+app.register(routes);
 
-app.register(routes)
-
-export default app
-
-
+export default app;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,12 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance } from 'fastify';
 
-async function routes(fastify: FastifyInstance): Promise<void>{
-
-    fastify.get('/health', async() => {
-        return {
-            status: 'OK',
-            message: 'Servidor DevBills rodando normalmente, Samuel'
-        }
-    })
+async function routes(fastify: FastifyInstance): Promise<void> {
+  fastify.get('/health', async () => {
+    return {
+      status: 'OK',
+      message: 'Servidor DevBills rodando normalmente, Samuel',
+    };
+  });
 }
 
-export default routes
+export default routes;
