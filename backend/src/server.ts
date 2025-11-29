d04d1472c4792ddeedb1933576d03e447b899a92
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-11-29 15:52:19 -0300

    implemented categories

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import app from './app';
 import { prismaConnect } from './config/prisma';
+import { initializeGlobalCategories } from './services/globalCategories.service';
 dotenv.config();
 
 const PORT = Number(process.env.PORT);
@@ -8,6 +9,8 @@ const PORT = Number(process.env.PORT);
 const startServer = async () => {
   try {
     await prismaConnect();
+
+    await initializeGlobalCategories();
 
     await app.listen({ port: PORT }).then(() => {
       console.log(`Server is running on port ${PORT}`);
--- backend/src/services/globalCategories.service.ts
+++ backend/src/services/globalCategories.service.ts
@@ -1,3 +1,8 @@
+import { TransactionType, type Category } from '@prisma/client';
+import prisma from '../config/prisma';
+
+type GlobalCategoryInput = Pick<Category, 'name' | 'color' | 'type'>;
+
 const globalCategories: GlobalCategoryInput[] = [
   // Despesas
   { name: 'Alimentação', color: '#FF5733', type: TransactionType.expense },
@@ -15,3 +20,33 @@ const globalCategories: GlobalCategoryInput[] = [
   { name: 'Investimentos', color: '#FFBA33', type: TransactionType.income },
   { name: 'Outros', color: '#B033FF', type: TransactionType.income },
 ];
+
+export const initializeGlobalCategories = async (): Promise<Category[]> => {
+  const createdCategories: Category[] = [];
+
+  for (const category of globalCategories) {
+    try {
+      const existing = await prisma.category.findFirst({
+        where: {
+          name: category.name,
+          type: category.type,
+        },
+      });
+      if (!existing) {
+        const newCategory = await prisma.category.create({
+          data: category,
+        });
+        console.log(`✅ Criada: ${newCategory.name}`);
+        createdCategories.push(newCategory);
+      } else {
+        createdCategories.push(existing);
+      }
+    } catch (err) {
+      console.error('🚨 Erro ao criar categorias');
+    }
+
+    console.log('✅ Todas as categorias Inicializadas.');
+  }
+
+  return createdCategories;
+};
