59a92a4d18601bf342e529650c9bd74536961b03
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-11-29 09:41:32 -0300

    conected sucessfully database with prisma

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,31 +1,13 @@
-// import { PrismaClient } from '@prisma/client';
-
-// // Inicializa o cliente Prisma
-// export const prisma = new PrismaClient();
-
-// export const prismaConnect = async () => {
-//   try {
-//     await prisma.$connect();
-//     console.log('✅ DB conectado com sucesso!');
-//   } catch (err) {
-//     console.error('🚨 Falha ao conectar o DB:', err);
-//     // Opcionalmente, você pode querer lançar o erro novamente para tratamento externo
-//     throw err;
-//   }
-// };
-
-
-import { PrismaClient } from "@prisma/client";
-
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export const prismaConnect = async () => {
   try {
     await prisma.$connect();
-  console.log("✅ DB conectado com sucesso");
+    console.log('✅ DB conectado com sucesso');
   } catch (error) {
-    console.log("🚨 Erro ao conectar ao servidor");
+    console.log('🚨 Erro ao conectar ao servidor');
   }
 };
 
