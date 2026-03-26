a7c4ade19e5ca90c44e2c344b76433c5afe293d8
Samuel Ferreira Inocêncio <154629333+SamuelInocencio@users.noreply.github.com>
2026-03-26 10:58:27 -0300

    feat: add MonthYearSelect component to Transactions page

--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -1,7 +1,32 @@
+import { Plus } from 'lucide-react';
+import { useState } from 'react';
+import { Link } from 'react-router';
+import MonthYearSelect from '../components/MonthYearSelect';
+
 const Transactions = () => {
+  const currentDate = new Date();
+  const [year, setYear] = useState<number>(currentDate.getFullYear());
+  const [month, setMonth] = useState<number>(currentDate.getMonth());
   return (
-    <div>
-      <h1>Transactions</h1>
+    <div className="container-app py-6">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
+        <h1 className="text-2xl font-bold mb-4 md:mb-0">Transações</h1>
+        <Link
+          to="/transacoes/nova"
+          className="bg-primary-500 text-[#051626] font-semibold px-4 py-2.5 rounded-xl flex items-center justify-center hover:bg-primary-600 transition-all"
+        >
+          <Plus className="w-4 h-4 mr-2" />
+          Nova Transação
+        </Link>
+      </div>
+      <div className="mb-6">
+        <MonthYearSelect
+          month={month}
+          year={year}
+          onMonthChange={setMonth}
+          onYearChange={setYear}
+        />
+      </div>
     </div>
   );
 };
