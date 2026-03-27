710f5dce94274195cb2fcfffd5562e4c8abdfdce
Samuel Ferreira Inocêncio <154629333+SamuelInocencio@users.noreply.github.com>
2026-03-27 13:51:47 -0300

    feat: finishing transactions screen implementation

--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -1,12 +1,12 @@
 import { AlertCircle, Plus, Search } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router';
+import Button from '../components/Button';
 import Card from '../components/Card';
 import Input from '../components/Input';
 import MonthYearSelect from '../components/MonthYearSelect';
 import { getTransactions } from '../services/transactionService';
 import type { Transaction } from '../types/transactions';
-import Button from '../components/Button';
 
 const Transactions = () => {
   const currentDate = new Date();
@@ -16,22 +16,22 @@ const Transactions = () => {
   const [error, setError] = useState<string>('');
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  useEffect(() => {
-    const fetchTransactions = async (): Promise<void> => {
-      try {
-        setLoading(true);
-        setError('');
-        const data = await getTransactions({ month, year });
-        setTransactions(data);
-      } catch (err) {
-        setError("Não foi possível carregar as transações, tente novamente.");
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchTransactions = async (): Promise<void> => {
+  //     try {
+  //       setLoading(true);
+  //       setError('');
+  //       const data = await getTransactions({ month, year });
+  //       setTransactions(data);
+  //     } catch (err) {
+  //       setError('Não foi possível carregar as transações, tente novamente.');
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchTransactions();
-  }, [month, year]);
+  //   fetchTransactions();
+  // }, [month, year]);
 
   return (
     <div className="container-app py-6">
@@ -65,16 +65,27 @@ const Transactions = () => {
 
       <Card className="overflow-hidden">
         {loading ? (
-          <div>Carregando...</div>
+          <div className="flex items-center justify-center">
+            <div className="w-10 h-10 border-4 border-primary-500 border-t-transparent rounded-full animate-spin" />
+          </div>
         ) : error ? (
           <div className="p-8 text-center">
             <AlertCircle className="w-12 h-12 text-red-500 mx-auto mb-4" />
             <p>{error}</p>
-            <Button>Tentar Novamente</Button>
+            <Button onClick={fetchTransactions} className="mx-auto mt-6">
+              Tentar Novamente
+            </Button>
           </div>
         ) : transactions?.length === 0 ? (
           <div className="text-center py-12">
             <p className="text-gray-500 mb-4">Nenhuma transação encontrada.</p>
+            <Link
+              to="/transacoes/nova"
+              className="w-fit mx-auto mt-6 bg-primary-500 text-[#051626] font-semibold px-4 py-2.5 rounded-xl flex items-center justify-center hover:bg-primary-600 transition-all"
+            >
+              <Plus className="w-4 h-4 mr-2" />
+              Nova Transação
+            </Link>
           </div>
         ) : (
           <div>Olá</div>
