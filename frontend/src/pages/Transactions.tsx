bc1f762986e26a6add5c622a984f475811af4d11
Samuel Ferreira Inocêncio <154629333+SamuelInocencio@users.noreply.github.com>
2026-03-27 13:31:57 -0300

    feat: continue transactions screen implementation

--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -1,13 +1,38 @@
-import { Plus, Search } from 'lucide-react';
-import { useState } from 'react';
+import { AlertCircle, Plus, Search } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router';
+import Card from '../components/Card';
 import Input from '../components/Input';
 import MonthYearSelect from '../components/MonthYearSelect';
+import { getTransactions } from '../services/transactionService';
+import type { Transaction } from '../types/transactions';
+import Button from '../components/Button';
 
 const Transactions = () => {
   const currentDate = new Date();
   const [year, setYear] = useState<number>(currentDate.getFullYear());
   const [month, setMonth] = useState<number>(currentDate.getMonth());
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async (): Promise<void> => {
+      try {
+        setLoading(true);
+        setError('');
+        const data = await getTransactions({ month, year });
+        setTransactions(data);
+      } catch (err) {
+        setError("Não foi possível carregar as transações, tente novamente.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTransactions();
+  }, [month, year]);
+
   return (
     <div className="container-app py-6">
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
@@ -20,15 +45,15 @@ const Transactions = () => {
           Nova Transação
         </Link>
       </div>
-      <div className="mb-6">
+      <Card className="mb-6">
         <MonthYearSelect
           month={month}
           year={year}
           onMonthChange={setMonth}
           onYearChange={setYear}
         />
-      </div>
-      <div className="mb-6">
+      </Card>
+      <Card className="mb-6">
         <Input
           id
           placeholder="Buscar transações..."
@@ -36,7 +61,25 @@ const Transactions = () => {
           fullWidth
           rror="Erro de pesquisa"
         />
-      </div>
+      </Card>
+
+      <Card className="overflow-hidden">
+        {loading ? (
+          <div>Carregando...</div>
+        ) : error ? (
+          <div className="p-8 text-center">
+            <AlertCircle className="w-12 h-12 text-red-500 mx-auto mb-4" />
+            <p>{error}</p>
+            <Button>Tentar Novamente</Button>
+          </div>
+        ) : transactions?.length === 0 ? (
+          <div className="text-center py-12">
+            <p className="text-gray-500 mb-4">Nenhuma transação encontrada.</p>
+          </div>
+        ) : (
+          <div>Olá</div>
+        )}
+      </Card>
     </div>
   );
 };
