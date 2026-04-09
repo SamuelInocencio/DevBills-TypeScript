9cdb8bd07deeb3bb40233cad09d3b3f84606df20
Samuel Ferreira Inocêncio <154629333+SamuelInocencio@users.noreply.github.com>
2026-04-09 11:30:06 -0300

    feat: implement Header and Footer components

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,16 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-gray-800 border-t border-gray-700 py-4">
+      <div className="container-app">
+        <p className="text-sm text-gray-400 text-center">
+          DevBills {currentYear} - Desenvolvido por{' '}
+          <strong>Samuel F. Inocêncio</strong> <strong> com TypeScript</strong> &{' '}
+          <strong>React</strong>
+        </p>
+      </div>
+    </footer>
   );
 };
 
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,71 @@
-import { Link } from "react-router";
+import { Activity } from 'lucide-react';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router';
+import { useAuth } from '../context/AuthContext';
+
+interface NavLink {
+  name: string;
+  path: string;
+}
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { authState, signOut } = useAuth();
+  const { pathname } = useLocation();
+
+  const isAuthenticated: boolean = !!authState.user;
+
+  const navLink: NavLink[] = [
+    { name: 'DashBoard', path: '/dashboard' },
+    { name: 'Transações', path: '/transacoes' },
+  ];
+
+  const renderAvatar = () => {
+    if (!authState.user) return null;
+
+    if (authState.user.photoURL) {
+      <img
+        src={authState.user.photoURL}
+        alt={`foto de perfil do(a) ${authState.user.displayName}`}
+        className="w-8 h-8 rounded-full border border-gray-700"
+      />;
+    }
+
+    return (
+      <div className="w-8 h-8 rounded-full bg-primary-500 flex items-center justify-center text-white font-medium">
+        {authState.user.displayName?.charAt(0)}
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h1>Header</h1>
-      <Link to="/transacoes">Transações</Link>
-    </div>
+    <header className="bg-gray-900 border-b border-gray-700">
+      <div className="container-app">
+        <div className="flex justify-between items-center py-4">
+          {/*LOGO*/}
+          <Link
+            to="/"
+            className="flex gap-2 text-xl text-primary-500 items-center font-bold"
+          >
+            <Activity className="h-6 w-6" />
+            DevBills
+          </Link>
+
+          {/*MENU DESKTOP */}
+
+          {isAuthenticated && (
+            <nav className='hidden md:flex space-x-3'>
+              {navLink.map((link) => (
+                <Link key={link.path} to={link.path}>
+                  {link.name}
+                </Link>
+              ))}
+            </nav>
+          )}
+        </div>
+      </div>
+    </header>
   );
 };
 
