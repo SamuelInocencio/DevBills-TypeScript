4010029dc48502c6350ef3143547adc761cbdf39
Samuel Ferreira Inocêncio <154629333+SamuelInocencio@users.noreply.github.com>
2026-04-09 12:14:13 -0300

    feat: implement Header components

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Activity } from 'lucide-react';
+import { Activity, LogIn, LogOut } from 'lucide-react';
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router';
 import { useAuth } from '../context/AuthContext';
@@ -21,15 +21,21 @@ const Header = () => {
     { name: 'Transações', path: '/transacoes' },
   ];
 
+  const handleSignOut = (): void => {
+    signOut();
+  };
+
   const renderAvatar = () => {
     if (!authState.user) return null;
 
     if (authState.user.photoURL) {
-      <img
-        src={authState.user.photoURL}
-        alt={`foto de perfil do(a) ${authState.user.displayName}`}
-        className="w-8 h-8 rounded-full border border-gray-700"
-      />;
+      return (
+        <img
+          src={authState.user.photoURL}
+          alt={`foto de perfil do(a) ${authState.user.displayName}`}
+          className="w-8 h-8 rounded-full border border-gray-700"
+        />
+      );
     }
 
     return (
@@ -55,14 +61,48 @@ const Header = () => {
           {/*MENU DESKTOP */}
 
           {isAuthenticated && (
-            <nav className='hidden md:flex space-x-3'>
+            <nav className="hidden md:flex space-x-3">
               {navLink.map((link) => (
-                <Link key={link.path} to={link.path}>
+                <Link
+                  key={link.path}
+                  to={link.path}
+                  className={
+                    pathname === link.path
+                      ? 'text-primary-500 bg-primary-500/10 rounded-md h-10 px-3 py-2'
+                      : 'text-gray-400 h-10 px-3 py-2 hover:text-primary-500 hover:bg-primary-500/5 rounded-md'
+                  }
+                >
                   {link.name}
                 </Link>
               ))}
             </nav>
           )}
+
+          <div className="hidden md:flex items-center space-x-4">
+            {isAuthenticated ? (
+              <div className="flex items-center space-x-4">
+                {/*AVATAR*/}
+                <div className="flex items-center space-x-2">
+                  {renderAvatar()}
+                  <span className="text-sm font-medium">
+                    {authState.user?.displayName}
+                  </span>
+                </div>
+
+                <button
+                  type="button"
+                  onClick={handleSignOut}
+                  className=" hover:text-red-300 hover:bg-red-500 p-2 rounded-full transition-colors cursor-pointer"
+                >
+                  <LogOut className="text-gray-200" />
+                </button>
+              </div>
+            ) : (
+              <Link to="/login">
+                <LogIn className="bg-primary-500 text-gray-900 font-semibold px-5 py-2.5 rounded-xl flex items-center justify-center hover:bg-primary-500 transition-all" />
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </header>
